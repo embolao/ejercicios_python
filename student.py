199439c10bb71ab1173bf58923a626987a9b2af1
embolao <corneigh@gmail.com>
2024-09-16 16:36:28 +0200

    Version 7

--- student.py
+++ student.py
@@ -1,15 +1,14 @@
-"""Collect a user's variables and puts them on the screen"""
+class Student:
+    ...
 def main():
     student = get_student()
-    if student["name"] == "Padma":
-        student["house"] = "Ravenclaw"
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
-    
+   student = Student()
+   student.name = input("Name: ")
+   student.house = input("House:")
+   return student
 
 if __name__ == "__main__":
     main()
