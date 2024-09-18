4bc751ec8b200b032b82d86350df6240f4bae39f
embolao <corneigh@gmail.com>
2024-09-18 10:59:25 +0200

    Version 8

--- student.py
+++ student.py
@@ -1,14 +1,16 @@
 class Student:
-    ...
+    def __init__(self, name, house):
+        self.name = name 
+        self.house = house
 def main():
     student = get_student()
     print(f"{student.name} from {student.house}")
 
 def get_student():
-   student = Student()
-   student.name = input("Name: ")
-   student.house = input("House:")
-   return student
+    name = input("Name: ")
+    house = input("House: ")
+    student = Student(name, house) #Crea objeto student
+    return student
 
 if __name__ == "__main__":
     main()
