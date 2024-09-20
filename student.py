55d5f9bae477bf2c7e7262de0f074b1c98c06383
embolao <corneigh@gmail.com>
2024-09-20 15:20:09 +0200

    Version 9

--- student.py
+++ student.py
@@ -1,16 +1,25 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor",
+                         "Hufflepuff",
+                         "Ravenclaw",
+                         "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name 
         self.house = house
+
+    def __str__(self):
+            return f"{self.name} from {self.house}"
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house) #Crea objeto student
-    return student
+    return Student(name, house)
 
 if __name__ == "__main__":
     main()
